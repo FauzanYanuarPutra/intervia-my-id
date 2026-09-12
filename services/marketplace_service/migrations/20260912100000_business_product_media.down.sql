634c1c0c75cd8aa0cf73fc405d07dcc28dc7dd47
Fauzan <90127994+FauzanYanuarPutra@users.noreply.github.com>
2026-09-12 18:07:21 +0700

    Remove colliding product media down migration version

--- services/marketplace_service/migrations/20260912100000_business_product_media.down.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE business_products
-  DROP CONSTRAINT IF EXISTS ck_business_products_image_dimensions,
-  DROP CONSTRAINT IF EXISTS ck_business_products_image_mime_type,
-  DROP CONSTRAINT IF EXISTS ck_business_products_image_url,
-  DROP CONSTRAINT IF EXISTS ck_business_products_image_complete,
-  DROP COLUMN IF EXISTS image_height,
-  DROP COLUMN IF EXISTS image_width,
-  DROP COLUMN IF EXISTS image_mime_type,
-  DROP COLUMN IF EXISTS image_url;
