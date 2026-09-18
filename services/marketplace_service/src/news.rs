0f907b63ab4c6b2313ab62ca3fd20bd696baf3c7
Fauzan <90127994+FauzanYanuarPutra@users.noreply.github.com>
2026-09-19 00:15:14 +0700

    test(news): reject oversized public cursors

--- services/marketplace_service/src/news.rs
+++ services/marketplace_service/src/news.rs
@@ -2541,5 +2541,6 @@ mod tests {
         let cursor = "2026-09-18T00:00:00+00:00|11111111-1111-1111-1111-111111111111";
         assert!(parse_news_cursor(Some(cursor)).unwrap().is_some());
         assert!(parse_news_cursor(Some("broken")).is_err());
+        assert!(parse_news_cursor(Some(&"x".repeat(97))).is_err());
     }
 }
